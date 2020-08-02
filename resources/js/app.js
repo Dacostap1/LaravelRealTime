05dc49e3adb695cc857ac2bf91a7aeadedd37fe9
Daniel Acosta <d4ni3L_15@hotmail.com>
2020-08-02 12:26:30 -0500

    mostrando los mensajes recibidos con laravel-echo

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,17 @@
 
 require('./bootstrap');
 
+window.Echo.channel('notifications').listen('UserSessionChanged', (e) => {
+    console.log(e.message)
+    const notificationElement = document.getElementById('notification');
+    notificationElement.innerText = e.message;
+    notificationElement.classList.remove('invisible');
+    notificationElement.classList.remove('alert-success');
+    notificationElement.classList.remove('alert-danger');
+
+    notificationElement.classList.add('alert-' + e.type);
+});
+
 window.Vue = require('vue');
 
 /**
